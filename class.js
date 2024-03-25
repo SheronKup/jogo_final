f251191e5a22e6f0a8f0621efee13f9666358c1c
Sheron <sheron_carneiro@estudante.sesisenai.org.br>
2024-03-25 08:11:31 -0300

    inicialização do jogo

---
+++ class.js
@@ -0,0 +1,80 @@
+class Obj {
+    constructor(x,y,w,h,at){
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.at = at
+    }
+
+des_obj(){
+
+}
+
+colid(objeto){
+
+}
+
+}
+
+class Nave extends Obj{
+
+
+
+    des_obj(){
+
+    }
+
+    anim(nome){
+
+    }
+
+    mov(){
+
+    }
+}
+
+class Disco2 extends Obj{
+    des_obj(){
+
+    }
+}
+
+class Disco3 extends Obj{
+    des_obj(){
+        
+    }
+}
+class Disco4 extends Obj{
+    des_obj(){
+        
+    }
+}
+class Disco5 extends Obj{
+    des_obj(){
+        
+    }
+}
+
+class Tiro extends Obj{
+    des_tiro(){
+
+    }
+
+    mov(){
+
+    }
+}
+
+class BG extends Obj{
+    mov(){
+
+    }
+}
+
+class Texto{
+    des_text(){
+        
+    }
+}
+   
---
+++ main.js
@@ -0,0 +1,21 @@
+function colisao(){
+    
+}
+
+function desenha(){    
+    
+
+}
+
+function atualiza(){
+ 
+}
+
+function main(){
+    des.clearRect(0,0,500,700)
+    desenha()
+    atualiza()
+    requestAnimationFrame(main)
+}
+
+main()
