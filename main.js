aae57e246c4cb8b99a2b18cb97a4f85ca3b1e1ec
Enzo Molinari Reis <enzoreis.tj@gmail.com>
2024-04-01 08:36:53 -0300

    feito game over e controles

--- main.js
+++ main.js
@@ -54,6 +54,55 @@ gameOver.volume = 1.0
 let podeAtirar = true
 let jogar = true
 
+function game_over(){
+    if(nav1.vida <= 0){
+        jogar = false
+        bg.pause()
+        kill.pause()
+        fire.pause()
+        gameOver.play()
+    }
+}
+
+let currentStage = 1
+let enemyDefeatedCount = 0 // Track defeated enemy count
+let totalEnemies = 0 // Placeholder, set this based on your enemy creation logic
+
+document.addEventListener('keydown', (ev)=>{
+    if(ev.key === 'a'){
+        nav1.isLeftPressed = true
+    }else if(ev.key === 'd'){
+        nav1.isRightPressed = true
+    }else if(ev.key === 'ArrowLeft'){
+        nav1.isLeftPressed = true
+    }else if(ev.key === 'ArrowRight'){
+        nav1.isRightPressed = true
+    }   
+})
+document.addEventListener('keyup', (ev)=>{
+    if(ev.key === 'a'){
+        nav1.isLeftPressed = false
+    }else if(ev.key === 'd'){
+        nav1.isRightPressed = false
+    }else if(ev.key === 'ArrowLeft'){
+        nav1.isLeftPressed = false
+    }else if(ev.key === 'ArrowRight'){
+        nav1.isRightPressed = false
+    }
+})
+
+
+
+document.addEventListener('keypress', (ev)=>{
+    if (ev.key === ' ' && podeAtirar && grupoTiros.length === 0) {
+        podeAtirar = false;
+        grupoTiros.push(new Tiro(nav1.x - 4 + nav1.w / 2, nav1.y, 8, 16, 'red'))
+        fire.play()
+        setTimeout(() => { podeAtirar = true; }, 1000)
+    }
+
+})
+
 function colisao(){
     
 }
